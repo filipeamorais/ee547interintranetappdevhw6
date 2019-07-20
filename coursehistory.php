7de8832ab75151618870a28ae504300f6ddb2af6
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-20 12:19:52 -0500

    new: removed unused code, formatted identation for php, css and html, changed some label contents and improved the css style.

--- coursehistory.php
+++ coursehistory.php
@@ -1,77 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Course History Database</title>
-  <link rel="stylesheet" href="demodb2.css" />
+  <link rel="stylesheet" href="basesearch.css" type="text/css">
 </head>
 
 <body>
-<h1>Select the course you want search the history</h1>
+  <main>
+    <div>
+      <h1>Course History</h1>
 
-<?PHP
-  
-  include '../appdata/eeschedule-db.inc';
+      <?PHP
 
-  $connection = mysqli_connect($host, $user, $password , $database);
+      include '../appdata/eeschedule-db.inc';
 
-  if (mysqli_connect_errno() ) {
-    die( mysqli_connect_error() );    // not production error handling
-  }
+      $connection = mysqli_connect($host, $user, $password, $database);
 
-  /**
-   * Make a drop down control
-   * 
-   * <select id='name' name='name_id'>
-   * <option value="from_row_id">from_row</option>
-   * ...
-   * </select>
-   */
+      if (mysqli_connect_errno()) {
+        die(mysqli_connect_error());    // not production error handling
+      }
 
-  function makeDropDown($name,$sql_rows) {
-    if (! $sql_rows) {
-      return "No Values for Control";
-    }
+      function makeDropDown($name, $sql_rows)
+      {
+        if (!$sql_rows) {
+          return "No Values for Control";
+        }
 
-    $name_id = $name . '_id';
-    $control = "<select id='$name' name='$name_id'>\n";
+        $name_id = $name . '_id';
+        $control = "<select id='$name' name='$name_id'>\n";
 
-    while ($row = mysqli_fetch_assoc($sql_rows)) {
-      $control .= ("  <option value =" . $row[$name] . ">" . $row[$name] . "</option>\n");
-    }
-    $control .= "</select>\n";
-    return $control;
-  }
+        while ($row = mysqli_fetch_assoc($sql_rows)) {
+          $control .= ("  <option value =" . $row[$name] . ">" . $row[$name] . "</option>\n");
+        }
+        $control .= "</select>\n";
+        return $control;
+      }
 
-  $sqlListCourses = "SELECT id, course FROM courses ORDER BY course ASC";
-  $sqlListInstructors = "SELECT id, instructor FROM instructors ORDER BY instructor ASC";
-  $result = mysqli_query($connection, $sqlListCourses);
-  $courseListDropDown = makeDropDown("course",$result);
-  mysqli_free_result($result);
-  $result = mysqli_query($connection, $sqlListInstructors);
-  $instructorListDropDown = makeDropDown("instructor",$result);
-  mysqli_free_result($result);
-  mysqli_close($connection);
-?>
+      $sqlListCourses = "SELECT id, course FROM courses ORDER BY course ASC";
+      $sqlListInstructors = "SELECT id, instructor FROM instructors ORDER BY instructor ASC";
+      $result = mysqli_query($connection, $sqlListCourses);
+      $courseListDropDown = makeDropDown("course", $result);
+      mysqli_free_result($result);
+      $result = mysqli_query($connection, $sqlListInstructors);
+      $instructorListDropDown = makeDropDown("instructor", $result);
+      mysqli_free_result($result);
+      mysqli_close($connection);
+      ?>
 
-  <h2>Add another offering to registration history</h2>
-  <form action="searchcourse.php" method="get">
-    <fieldset>
-      <!-- <span class="required">* <small>required value</small></span><br />
-      <label for="semester">Semester<span class="required">*</span></label> <input type="text" id="semester" name="semester" required placeholder="2019-3Summer" /><br /> -->
-      <label for="course_id">Course<span class="required">*</span></label> <?PHP echo $courseListDropDown ?><br />
-      <!-- <label for="sec">Section<span class="required">*</span></label> <input type="text" id="sec" name="sec" placeholder="OY" required /><br />
-      <label for="days">Days<span class="required">*</span></label> <input type="text" id="days" name="days" placeholder="TR" required /><br />
-      <label for="time">Time<span class="required">*</span></label> <input type="text" id="time" name="time" placeholder="05:20 pm-07:20 pm" required /><br />
-      <label for="cap">Capacity<span class="required">*</span></label> <input type="number" id="cap" name="cap" placeholder="25" required /><br />
-      <label for="act">Actual<span class="required">*</span></label> <input type="number" id="act" name="act"  placeholder="25" required /><br />
-      <label for="instructor_id">Instructor<span class="required">*</span></label> <?PHP echo $instructorListDropDown ?><br />
-      <label for="location">Location<span class="required">*</span></label> <input type="text" id="location" name="location" placeholder="BEC 155" required /><br /> -->
-      <input type="submit">
-    </fieldset>
-  </form>
+      <h2>Select the course you want to search the history:</h2>
+      <form action="searchcourse.php" method="get">
+        <fieldset>
+          <label for="course_id">Course<span class="required">*</span></label> <?PHP echo $courseListDropDown ?><br />
+          <input type="submit">
+        </fieldset>
+      </form>
 
 </body>
+
 </html>
--- searchcourse.php
+++ searchcourse.php
@@ -1,53 +1,60 @@
 <?PHP
-  include '../appdata/eeschedule-db.inc';
+include '../appdata/eeschedule-db.inc';
 
-  $connection = mysqli_connect($host, $user, $password , $database);
+$connection = mysqli_connect($host, $user, $password, $database);
 
-  if (mysqli_connect_errno() ) {
-    die( mysqli_connect_error() );    // not production error handling
-  }
+if (mysqli_connect_errno()) {
+  die(mysqli_connect_error());    // not production error handling
+}
 ?>
 
+<link rel="stylesheet" href="basesearch.css" type="text/css">
+
 <body>
-  <h1>Add offering</h1>
+  <main>
+    <h1>Course History</h1>
+  </main>
   <p>
-    The following offering was submitted.
+    Follow your selected course history bellow:
   </p>
   <table>
     <tr>
-      <th>Semester</th><th>Course</th><th>Title</th><th>Days</th><th>Time</th><th>Enrolled(Act)</th><th>Instructor</th><th>Location</th>
+      <th>Semester</th>
+      <th>Course</th>
+      <th>Title</th>
+      <th>Days</th>
+      <th>Time</th>
+      <th>Enrolled(Act)</th>
+      <th>Instructor</th>
+      <th>Location</th>
     </tr>
-<?php
-echo "    <tr>\n";
-$fieldName = "course_id";
-$courseId = $_GET[$fieldName];
-echo $courseId;
-$sqlSelectCourse = "SELECT * FROM courses WHERE (course='" . $courseId . "')";
-echo $sqlSelectCourse;
-$resultCourse = mysqli_query($connection, $sqlSelectCourse);
-$courseRow = mysqli_fetch_assoc($resultCourse);
-$sqlSelectReginfo = "SELECT * FROM reginfo WHERE (course_id='" . $courseRow['id'] . "')";
-echo $sqlSelectReginfo;
-$resultReginfo = mysqli_query($connection, $sqlSelectReginfo);
-$reginfoRow = mysqli_fetch_assoc($resultReginfo);
-$sqlSelectInstructor = "SELECT * FROM instructors WHERE (id='" . $reginfoRow['instructor_id'] . "')";
-echo $sqlSelectInstructor;
-$resultInstructor = mysqli_query($connection, $sqlSelectInstructor);
-$instructorRow = mysqli_fetch_assoc($resultInstructor);
-while ($reginfoRow = mysqli_fetch_assoc($resultReginfo)) {
-echo "      <td>" .$reginfoRow['semester']. "</td>";
-echo "      <td>" .$courseRow['course']. "</td>";
-echo "      <td>" .$courseRow['title']. "</td>";
-echo "      <td>" .$reginfoRow['days']. "</td>";
-echo "      <td>" .$reginfoRow['time']. "</td>";
-echo "      <td>" .$reginfoRow['act']. "</td>";
-echo "      <td>" .$instructorRow['instructor']. "</td>";
-echo "      <td>" .$reginfoRow['location']. "</td>";
-echo "    </tr>\n";
-}
-?>
-    </table>
+    <?php
+    echo "    <tr>\n";
+    $fieldName = "course_id";
+    $courseId = $_GET[$fieldName];
+    $sqlSelectCourse = "SELECT * FROM courses WHERE (course='" . $courseId . "')";
+    $resultCourse = mysqli_query($connection, $sqlSelectCourse);
+    $courseRow = mysqli_fetch_assoc($resultCourse);
+    $sqlSelectReginfo = "SELECT * FROM reginfo WHERE (course_id='" . $courseRow['id'] . "')";
+    $resultReginfo = mysqli_query($connection, $sqlSelectReginfo);
+    $reginfoRow = mysqli_fetch_assoc($resultReginfo);
+    $sqlSelectInstructor = "SELECT * FROM instructors WHERE (id='" . $reginfoRow['instructor_id'] . "')";
+    $resultInstructor = mysqli_query($connection, $sqlSelectInstructor);
+    $instructorRow = mysqli_fetch_assoc($resultInstructor);
+    while ($reginfoRow = mysqli_fetch_assoc($resultReginfo)) {
+      echo "      <td>" . $reginfoRow['semester'] . "</td>";
+      echo "      <td>" . $courseRow['course'] . "</td>";
+      echo "      <td>" . $courseRow['title'] . "</td>";
+      echo "      <td>" . $reginfoRow['days'] . "</td>";
+      echo "      <td>" . $reginfoRow['time'] . "</td>";
+      echo "      <td>" . $reginfoRow['act'] . "</td>";
+      echo "      <td>" . $instructorRow['instructor'] . "</td>";
+      echo "      <td>" . $reginfoRow['location'] . "</td>";
+      echo "    </tr>\n";
+    }
+    ?>
+  </table>
 
-    <p>
+  <p>
     <a href="coursehistory.php">Make another search</a>
   </p>
