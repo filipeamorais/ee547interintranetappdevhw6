f4d0a9c55c2f02a71b39c406c6b08efe8265ce17
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-21 22:47:54 -0500

    new: added changes to avoid XSS.

--- searchcourse.php
+++ searchcourse.php
@@ -27,7 +27,7 @@ if ($orderBy == "reginfo.semester") {
     <h1>Course History</h1>
   </main>
   <p>
-    Follow bellow your selected course history sorted first by <b> <?PHP echo $orderByLabel ?> </b> and then by <b> <?PHP echo $secondOrderByLabel ?> </b> in descending order:
+    Follow bellow your selected course history sorted first by <b> <?PHP echo htmlspecialchars($orderByLabel, ENT_QUOTES, 'UTF-8') ?> </b> and then by <b> <?PHP echo htmlspecialchars($secondOrderByLabel, ENT_QUOTES, 'UTF-8') ?> </b> in descending order:
   </p>
   <table>
     <tr>
@@ -42,15 +42,8 @@ if ($orderBy == "reginfo.semester") {
     </tr>
     <?php
     echo "    <tr>\n";
-    $sqlSelectDataInnerJoin = "SELECT * FROM courses INNER JOIN reginfo ON courses.id=course_id INNER JOIN instructors ON instructor_id=instructors.id WHERE course='" . $courseId . "' ORDER BY " . $orderBy . " DESC, " . $secondOrderBy . " DESC";
+    $sqlSelectDataInnerJoin = "SELECT * FROM courses INNER JOIN reginfo ON courses.id=course_id INNER JOIN instructors ON instructor_id=instructors.id WHERE course='" . htmlspecialchars($courseId, ENT_QUOTES, 'UTF-8') . "' ORDER BY " . htmlspecialchars($orderBy, ENT_QUOTES, 'UTF-8') . " DESC, " . htmlspecialchars($secondOrderBy, ENT_QUOTES, 'UTF-8') . " DESC";
     $resultCourseHistory = mysqli_query($connection, $sqlSelectDataInnerJoin);
-    // $courseHistoryRow = mysqli_fetch_assoc($resultCourseHistory);
-    // $sqlSelectReginfo = "SELECT * FROM reginfo WHERE (course_id='" . $courseRow['id'] . "')";
-    // $resultReginfo = mysqli_query($connection, $sqlSelectReginfo);
-    // $reginfoRow = mysqli_fetch_assoc($resultReginfo);
-    // $sqlSelectInstructor = "SELECT * FROM instructors WHERE (id='" . $reginfoRow['instructor_id'] . "')";
-    // $resultInstructor = mysqli_query($connection, $sqlSelectInstructor);
-    // $instructorRow = mysqli_fetch_assoc($resultInstructor);
     while ($courseHistoryRow = mysqli_fetch_assoc($resultCourseHistory)) {
       echo "      <td>" . $courseHistoryRow['semester'] . "</td>";
       echo "      <td>" . $courseHistoryRow['course'] . "</td>";
